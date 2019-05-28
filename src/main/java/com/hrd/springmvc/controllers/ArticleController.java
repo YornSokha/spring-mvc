55d84aae98664ecb2f92e33906373ed1b2853349
Sokha <yornsokha168@gmail.com>
2019-05-28 23:07:17 +0700

    add requestParam and pathVariable

--- src/main/java/com/hrd/springmvc/controllers/ArticleController.java
+++ src/main/java/com/hrd/springmvc/controllers/ArticleController.java
@@ -5,6 +5,8 @@ import com.hrd.springmvc.service.article_service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ArticleController {
@@ -15,6 +17,19 @@ public class ArticleController {
     public String index(){
         System.out.println(articleService.findAll());
         return "index";
+    }
+
+    @GetMapping("/request")
+    public String request(@RequestParam String name){
+        System.out.println("Name : " + name);
+        return "redirect:/";
+    }
+
+    @GetMapping("/path/{name}/{id}")
+    public String path(@PathVariable String name, @PathVariable int id){
+        System.out.println("ID : " + id);
+        System.out.println("Name : " + name);
+        return "redirect:/";
     }
 
     @GetMapping("/add")
